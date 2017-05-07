8c99b11d441496b8c9b22969162caa5ab2311dee
Andreas Schabus <schabus@hotmail.com>
2017-05-07 10:43:37 +0200

    change web site title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Andi's Homepage aus Wien" });
+  res.render('index', { title: "Andi's Homepage von Wien" });
 });
 
 module.exports = router;
