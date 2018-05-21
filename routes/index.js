1da21abc6154dab238eda8f41192055e6fa62733
Andreas Schabus <schabus@hotmail.com>
2018-05-21 21:32:30 +0200

    Fix title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Andi's Homepage fuer den 3.Bezirk!" });
+  res.render('index', { title: "Andi's Homepage fuer den 12.Bezirk!" });
 });
 
 module.exports = router;
